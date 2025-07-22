be2be3042d11d635450ee0bb0dc448a630417103
belatoledo <it.amancio@gmail.com>
2025-07-22 12:52:11 -0300

    feat: setup jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
+// eslint.config.mjs
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
 import prettierConfig from 'eslint-config-prettier';
 import importHelpers from 'eslint-plugin-import-helpers';
+import jest from 'eslint-plugin-jest';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,6 +14,7 @@ const compat = new FlatCompat({
   resolvePluginsRelativeTo: __dirname,
 });
 
+/** @type {import('eslint').Linter.FlatConfig[]} */
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
   {
@@ -19,6 +22,16 @@ export default [
   },
   ...compat.extends('next/core-web-vitals'),
   js.configs.recommended,
+
+  {
+    ...jest.configs['flat/recommended'],
+    files: ['**/*.test.js', '**/*.test.jsx', '**/*.test.ts', '**/*.test.tsx'],
+    rules: {
+      ...jest.configs['flat/recommended'].rules,
+      'jest/expect-expect': 'off',
+    },
+  },
+
   {
     files: ['src/**/*.{js,jsx,ts,tsx}'],
     plugins: {
