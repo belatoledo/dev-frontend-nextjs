a3c45e9702ad5e9d05d99ea110c4aa69c25aa1db
belatoledo <it.amancio@gmail.com>
2025-07-25 05:20:28 -0300

    feat: create login schema

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,5 +1,12 @@
 import { z } from 'zod';
 
+export const loginSchema = z.object({
+  email: z.string().email('Por favor, insira um e-mail válido.'),
+  password: z.string().min(1, 'A senha é obrigatória.'),
+});
+
+export type LoginFormData = z.infer<typeof loginSchema>;
+
 export const productSchema = z.object({
   title: z.string().min(3, 'O título deve ter pelo menos 3 caracteres.'),
   price: z.coerce.number().positive('O preço deve ser um número positivo.'),
